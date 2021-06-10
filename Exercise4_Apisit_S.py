184ca4ff4db6762e2433ddc12af1f57c38b148ba
ZzMEGAzZ <85662757+ZzMEGAzZ@users.noreply.github.com>
2021-06-10 11:58:43 +0700

    Create Exercise4_Apisit_S.py
     Exercise 4 : ปฏิบัติการทดสอบการใช้งานตัวแปร

---
+++ Exercise4_Apisit_S.py
@@ -0,0 +1,10 @@
+FE = 90
+GB = 65.5
+ICS = 75
+CP = 69.5
+
+print("---Your Score---")
+print("Foundation English :",FE)
+print("General Business :",GB)
+print("Introduction to Computer System :",ICS)
+print("Computer Programming :",CP)
